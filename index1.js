73dced2d137b2326792b4e35d19487fb1b5b8189
SAI KRISHNAN.S <saikrishnan.s2024cse@sece.ac.in>
2024-11-27 15:19:09 +0000

    Refactor navigation and creators section; implement side navigation and update layout

--- index1.js
+++ index1.js
@@ -1,11 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 /*feedback*/
 document.getElementById('feedbackForm').addEventListener('submit', function(event) {
     event.preventDefault();
