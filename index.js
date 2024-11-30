4a87965a06090388e9f9681d1b73d2e1c9d5c3e5
SAI KRISHNAN.S <saikrishnan.s2024cse@sece.ac.in>
2024-12-01 02:11:53 +0530

    all file are changed and get refreshed

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-/*feedback*/
-document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const feedback = document.getElementById('feedback').value.trim();
-    
-    if (name === '' || email === '' || feedback === '') {
-        alert('Please fill in all fields.');
-        return;
-    }
-    
-    if (!validateEmail(email)) {
-        alert('Please enter a valid email address.');
-        return;
-    }
-    
-    alert('Feedback submitted successfully!');
-    // Here you can add code to send the form data to the server
-});
-
-function validateEmail(email) {
-    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return re.test(String(email).toLowerCase());
-}
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
